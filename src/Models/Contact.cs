003ec275a0d464649f8a27499896286a98b272d3
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-23 16:27:37 +0300

    Add the Contact model

---
+++ src/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "Name cannot be over 100 characters")]
+        public string Name { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public bool Married { get; set; }
+
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Range(0.0, Double.MaxValue, ErrorMessage = "Salary cannot be lower than 0")]
+        public decimal Salary { get; set; }
+    }
+}

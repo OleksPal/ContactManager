db78bd2866bb1a079f966cb05c07f2425ce841c4
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-23 18:56:34 +0300

    Configure services in mvc project and add a connection string

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,6 @@
+using ContactManager.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace ContactManager
 {
     public class Program
@@ -8,6 +11,9 @@ namespace ContactManager
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ContactManagerContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 

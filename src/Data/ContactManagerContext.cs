a5b071c6b2357abfa270cb5b0f480788711d7be0
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-23 18:34:54 +0300

    Add Db context class

---
+++ src/Data/ContactManagerContext.cs
@@ -0,0 +1,12 @@
+﻿using ContactManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager.Data
+{
+    public class ContactManagerContext : DbContext
+    {
+        public ContactManagerContext(DbContextOptions<ContactManagerContext> options) : base(options) { }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}

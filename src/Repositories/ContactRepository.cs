b400c175c8d8e050a933a4c8403d80a5e1876b66
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-24 13:56:01 +0300

    Implement the IContactRepository

---
+++ src/Repositories/ContactRepository.cs
@@ -0,0 +1,28 @@
+﻿using ContactManager.Data;
+using ContactManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager.Repositories
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly ContactManagerContext _context;
+
+        public ContactRepository(ContactManagerContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Contact>> GetAllAsync()
+        {
+            return await _context.Contacts.ToListAsync();
+        }
+
+        public async Task<ICollection<Contact>> InsertRangeAsync(IEnumerable<Contact> contacts)
+        {
+            await _context.Contacts.AddRangeAsync(contacts);
+
+            return contacts.ToList();
+        }
+    }
+}

ea6a3765ab5232a32b6131e3297d50011fabd396
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-24 13:45:02 +0300

    Add an interface for the contact repository

---
+++ src/Repositories/IContactRepository.cs
@@ -0,0 +1,10 @@
+﻿using ContactManager.Models;
+
+namespace ContactManager.Repositories
+{
+    public interface IContactRepository
+    {
+        Task<ICollection<Contact>> GetAllAsync();
+        Task<ICollection<Contact>> InsertRangeAsync(IEnumerable<Contact> contacts);
+    }
+}

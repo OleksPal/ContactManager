822aca07804ae2c620b617bcfe2a6ddd2c2b7191
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-24 14:29:02 +0300

    Add an update and delete methods for the contact repository

--- src/Repositories/ContactRepository.cs
+++ src/Repositories/ContactRepository.cs
@@ -25,5 +25,36 @@ namespace ContactManager.Repositories
 
             return contacts.ToList();
         }
+
+        public async Task<Contact> UpdateAsync(Guid id, ContactCsvRecord record)
+        {
+            var contact = await _context.Contacts.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (contact is null)
+                return null;
+
+            contact.Name = record.Name;
+            contact.DateOfBirth = record.DateOfBirth;
+            contact.Married = record.Married;
+            contact.Phone = record.Phone;
+            contact.Salary = record.Salary;
+
+            await _context.SaveChangesAsync();
+
+            return contact;
+        }
+
+        public async Task<Contact> DeleteAsync(Guid id)
+        {
+            var contact = await _context.Contacts.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (contact is null)
+                return null;
+
+            _context.Contacts.Remove(contact);
+            await _context.SaveChangesAsync();
+
+            return contact;
+        }           
     }
 }
--- src/Repositories/IContactRepository.cs
+++ src/Repositories/IContactRepository.cs
@@ -6,5 +6,7 @@ namespace ContactManager.Repositories
     {
         Task<ICollection<Contact>> GetAllAsync();
         Task<ICollection<Contact>> InsertRangeAsync(IEnumerable<Contact> contacts);
+        Task<Contact> UpdateAsync(Guid id, ContactCsvRecord contact);
+        Task<Contact> DeleteAsync(Guid id);
     }
 }

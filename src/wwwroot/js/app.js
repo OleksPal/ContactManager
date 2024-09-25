3b39341e34fd8aa11931e75f95ba3ff232566223
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-25 17:17:57 +0300

    Add contacts sorting

--- src/wwwroot/js/app.js
+++ src/wwwroot/js/app.js
@@ -1,5 +1,9 @@
 ﻿var contactManagerApp = angular.module("contactManagerApp", []);
 
+angular.module("contactManagerApp").config(function () {
+    angular.lowercase = angular.$$lowercase;
+});
+
 contactManagerApp.controller("ContactManagerController", ["$scope", "$http", function ($scope, $http) {
 
     $http.get("/Contact/GetAllContacts/").then(successCallback, errorCallback);
@@ -13,4 +17,17 @@ contactManagerApp.controller("ContactManagerController", ["$scope", "$http", fun
 
     $scope.searchWord = {};
     $scope.contactFields = ["name", "dateOfBirth", "married", "phone", "salary"];
+
+    $scope.order = '';
+    $scope.changeOrder = function (value) {
+        if ($scope.order === value) {
+            $scope.order = '-' + value;
+        }
+        else if ($scope.order === '-' + value) {
+            $scope.order = value;
+        }
+        else {
+            $scope.order = value;
+        }
+    }
 }])
